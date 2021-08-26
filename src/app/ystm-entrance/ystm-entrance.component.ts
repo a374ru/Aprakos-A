37b003e6914ed6f0d69a23a1ad3fed4aafc3629a
Oleg <ibo7@me.com>
2021-08-27 01:58:01 +0300

    Start material

--- src/app/ystm-entrance/ystm-entrance.component.ts
+++ src/app/ystm-entrance/ystm-entrance.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YstmEntranceComponent implements OnInit {
 
+  entrance: string ="Ми1лости про1симъ во а4нгулzръ в7i.";
+  bukvica: string = "†"
+  numberok: string = "†"
+  imageCross: string = `https://1.bp.blogspot.com/-s_FOvMb40sg/Xp2eR4kbPVI/AAAAAAAAFJk/NG0rHfdgKHgWKlb5me5r-0J8_LdC82AywCLcBGAsYHQ/s222/contacts-%2540.png`
+
   constructor() { }
 
   ngOnInit(): void {
+    this.kinovar(this.entrance)
+  }
+
+  kinovar(str: string = "No string"){
+    this.bukvica = str.slice(0,1) as string;
+    this.numberok = str.slice(-4) as string;
+    this.entrance = str.slice(1,(str.length-4))
   }
 
 }
