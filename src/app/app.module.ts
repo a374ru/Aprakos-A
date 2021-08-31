85145e2b60694a4dbb968b7fa6879ce418b0fc7a
Oleg <ibo7@me.com>
2021-08-31 14:35:44 +0300

    333

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YstmEntranceComponent } from './ystm-entrance/ystm-entrance.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCard } from '@angular/material/mat-card';
 @NgModule({
   declarations: [
     AppComponent,
-    YstmEntranceComponent
+    YstmEntranceComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MatGridListModule,
+    MatCard
   ],
   providers: [],
   bootstrap: [AppComponent]
