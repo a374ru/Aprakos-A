ae0b5607aad23fe2c04d3b91b00952f729b8b43a
Oleg <ibo7@me.com>
2021-09-18 18:31:16 +0300

    777

--- src/app/apr/apr.component.ts
+++ src/app/apr/apr.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TimeboxService } from '../services/timebox.service';
 import { aprakos } from 'src/app/apr/aprakos';
+import { regExpEscape } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-apr',
@@ -16,7 +17,9 @@ export class AprComponent implements OnInit {
   apstl = this.aprakos[0].apstl
   // let zevngl = ""
   evngl = this.aprakos[0].evngl
-
+  
+  buckvicaApstl = "???"
+  buckvicaEvngl = "???"
 
   // for (const i of  array) {
     
@@ -24,12 +27,33 @@ export class AprComponent implements OnInit {
 
   constructor(private timeBox: TimeboxService) { 
 
-    console.log(this.aprakos[0].apstl);
+    // console.log(this.aprakos[0].apstl);
+
+
 
 
   }
 
-  ngOnInit(): void {    }
+  ngOnInit(): void {  
+
+    let regx =   /\(За\?\s\S*\)\s(\S|Ё|)?#?:?\$?/
+
+    let rng = aprakos[0].apstl.match(regx)
+    this.apstl = aprakos[0].apstl.slice(rng![0].length)
+    this.buckvicaApstl = rng![0]
+    
+    let rng2 = aprakos[0].evngl.match(regx)
+    this.evngl = aprakos[0].evngl.slice(rng2![0].length)
+    this.buckvicaEvngl = rng2![0]
+  
+
+console.log(rng![0]);
+
+    
+
+
+    }
+
 
 
 
