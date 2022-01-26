7d43c02c56dac497fd70ed4da5c359e0f50fbfd4
Oleg <ibo7@me.com>
2022-01-26 12:30:35 +0300

    777

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,58 +6,50 @@ import { DateYearService } from './services/date-year.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   template: '<app-apr [revertColorText]="revertColor"></app-apr> ',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
-
-
   constructor(public dateyearService: DateYearService) {
-
-    localStorage.setItem('ystm', 'Yabo-system © Third Millennium')
-
-
+    localStorage.setItem('ystm', 'Yabo-system © Third Millennium');
   }
 
-
   title = 'Angular-Aprakos';
-  bgcolor = "#FFF5EE"; // seashell
-  currentYearCSL = "Not YearCSL";
+  bgcolor = '#FFF5EE'; // seashell
+  currentYearCSL = 'Not YearCSL';
+
+  // TagsEE: теги CMS ExpressionEngine
+  siteName = '{site_name}';
 
   ngOnInit() {
-    this.currentYearCSL = this.dateyearService.currentYearCSl()
+    this.currentYearCSL = this.dateyearService.currentYearCSl();
 
     // let cookieYstm = document.cookie.split(',')
-  var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)themes\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    var cookieValue = document.cookie.replace(
+      /(?:(?:^|.*;\s*)themes\s*\=\s*([^;]*).*$)|^.*$/,
+      '$1'
+    );
 
-    if (cookieValue == "black") {
-      this.changeBGColor()
-
+    if (cookieValue == 'black') {
+      this.changeBGColor();
     } else {
-      document.getElementById('swapColor')!.innerHTML = this.currentYearCSL
+      document.getElementById('swapColor')!.innerHTML = this.currentYearCSL;
     }
-
   }
 
   changeBGColor() {
-
-    let white: string = "seashell"
-    let black: string = "#251D25"
+    let white: string = 'seashell';
+    let black: string = '#251D25';
 
     if (this.bgcolor == black) {
-      this.bgcolor = white
+      this.bgcolor = white;
       document.body.style.backgroundColor = this.bgcolor;
-      document.getElementById('swapColor')!.innerHTML = "де1нь"
-      document.cookie = "themes=white"
-
+      document.getElementById('swapColor')!.innerHTML = 'де1нь';
+      document.cookie = 'themes=white';
     } else {
-      
       document.body.style.backgroundColor = black;
-      document.getElementById('swapColor')!.innerHTML = "но1чь"
-      document.cookie = "themes=black"
-      this.bgcolor = black
+      document.getElementById('swapColor')!.innerHTML = 'но1чь';
+      document.cookie = 'themes=black';
+      this.bgcolor = black;
     }
-
-
   }
 }
--- src/app/apr/apr.component.ts
+++ src/app/apr/apr.component.ts
@@ -4,17 +4,14 @@ import { TimeboxService } from '../services/timebox.service';
 @Component({
   selector: 'app-apr',
   templateUrl: './apr.component.html',
-  styleUrls: ['./apr.component.scss']
+  styleUrls: ['./apr.component.scss'],
 })
-
-
 export class AprComponent implements OnInit {
-
   /**
    * Апракос в формате JSON.
    * Данный json не входит в состав программы, составляется и корретируется в другом проекте. Данный JSON файл является оригинальным поддерживаемым документом.
    */
-  url = "https://cdn.jsdelivr.net/gh/a374ru/aprakos-json@master/aprakos.json"
+  url = 'https://cdn.jsdelivr.net/gh/a374ru/aprakos-json@master/aprakos.json';
 
   /**
    * Название текущего дня
@@ -24,35 +21,38 @@ export class AprComponent implements OnInit {
   /**
    * Седмица по Пасхе
    */
-  weekFromEaster = this.timeBox.formatsEaster.currentWeek as number
+  weekFromEaster = this.timeBox.formatsEaster.currentWeek as number;
 
   /**
-   * Текущая седмица по Уставу. 
+   * Текущая седмица по Уставу.
    * Может быть седмицей от Пасхи до Пятидесятницы.
    * Может быть седмицей от Пятидесятницы до Пасхи.
    * Корректировка номера текущей седмицы. Зависит от части года (По Пасхе или по Пятьдесятнице)
    */
-  currentWeek = this.weekFromEaster < 8 ? ` по Пасхе ${this.weekFromEaster}` : ` по Пятьдесятнице: ${this.weekFromEaster - 7}`;
+  currentWeek =
+    this.weekFromEaster < 8
+      ? ` по Пасхе ${this.weekFromEaster}`
+      : ` по Пятьдесятнице: ${this.weekFromEaster - 7}`;
 
   /**
    * Апостолтекущего дня.
-  */
-  apstl?: string
+   */
+  apstl?: string;
 
   /**
    * Евангелие текущего дня.
-  */
-  evngl?: string
+   */
+  evngl?: string;
 
   /**
-   * Зачало и Буквица для Апостола. 
+   * Зачало и Буквица для Апостола.
    */
-  buckvicaApstl?: string
+  buckvicaApstl?: string;
 
   /**
-   * Зачало и Буквица для Евангелия. 
+   * Зачало и Буквица для Евангелия.
    */
-  buckvicaEvngl?: string
+  buckvicaEvngl?: string;
 
   /**
    * Номер зачала и стиха текущего Апракоса для Апостола
@@ -60,7 +60,7 @@ export class AprComponent implements OnInit {
   zapstl?: string;
   stupka?: number;
   /**
-   * Номер зачала и стиха текущего Апракоса для Евангелия. 
+   * Номер зачала и стиха текущего Апракоса для Евангелия.
    */
   zevngl?: string;
 
@@ -68,66 +68,60 @@ export class AprComponent implements OnInit {
   date: string;
 
   constructor(private timeBox: TimeboxService) {
-  try {
-      
-    this.date = this.timeBox.theMoment.getDate() + " " + this.timeBox.formatsEaster.currentMonth
-    this.yearEaster = this.timeBox.theMoment.getFullYear() + "" 
+    try {
+      this.date =
+        this.timeBox.theMoment.getDate() +
+        ' ' +
+        this.timeBox.formatsEaster.currentMonth;
+      this.yearEaster = this.timeBox.theMoment.getFullYear() + '';
       // Получение aprakos.json с сервера.
-      fetch(this.url).then(loadedJson => loadedJson.text()).then(loadedJsonText => this.currentZachalo(JSON.parse(loadedJsonText))).catch(() =>
-        alert("Неудачная загрузка. Обновите страницу…")
-      )
-
+      fetch(this.url)
+        .then((loadedJson) => loadedJson.text())
+        .then((loadedJsonText) =>
+          this.currentZachalo(JSON.parse(loadedJsonText))
+        )
+        .catch(() => alert('Неудачная загрузка. Обновите страницу…'));
     } finally {
       // this.apstl = "Загрузка..."
-      this.evngl = "Загрузка..."
+      this.evngl = 'Загрузка...';
     }
-
   }
 
-
   /**
-   * Запускает загрузку `json` с другого сервера. 
+   * Запускает загрузку `json` с другого сервера.
    * Вызывает функцию `currentZachalo()`.
    */
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   /**
-   * Функция вычисляет текущее зачало из файла aprakos.json и выполняет поиск по регулярному выражению номера стиха и первой буквы зачал Апостола и Евангелия для выделения киноварью в шаблоне HTML. 
+   * Функция вычисляет текущее зачало из файла aprakos.json и выполняет поиск по регулярному выражению номера стиха и первой буквы зачал Апостола и Евангелия для выделения киноварью в шаблоне HTML.
    */
   currentZachalo(json: any) {
-
-    this.stupka = this.timeBox.formatsEaster.vozStupka as number
-    console.log(this.stupka)
-    
+    this.stupka = this.timeBox.formatsEaster.vozStupka as number;
+    console.log(this.stupka);
 
     for (const key in json) {
-      const curApr = "" + (this.timeBox.formatsEaster.currentWeek as number - this.stupka) + (this.timeBox.formatsEaster.dayNum)
+      const curApr =
+        '' +
+        ((this.timeBox.formatsEaster.currentWeek as number) - this.stupka) +
+        this.timeBox.formatsEaster.dayNum;
       if (json[key].aprakos == curApr) {
-        this.zapstl = json[key].zapstl
-        this.apstl = json[key].apstl
-        this.zevngl = json[key].zevngl
-        this.evngl = json[key].evngl
+        this.zapstl = json[key].zapstl;
+        this.apstl = json[key].apstl;
+        this.zevngl = json[key].zevngl;
+        this.evngl = json[key].evngl;
 
         // Регулярное выражение для поиска нумерации стихов и БУКВИЦЫ
-        let regx = /\(За\?\s\S*\)\s(\S|Ё|)?#?:?\$?/
+        let regx = /\(За\?\s\S*\)\s(\S|Ё|)?#?:?\$?/;
 
-        let rng = json[key].apstl.match(regx)
-        this.apstl = json[key].apstl.slice(rng![0].length)
-        this.buckvicaApstl = rng![0]
+        let rng = json[key].apstl.match(regx);
+        this.apstl = json[key].apstl.slice(rng![0].length);
+        this.buckvicaApstl = rng![0];
 
-        let rng2 = json[key].evngl.match(regx)
-        this.evngl = json[key].evngl.slice(rng2![0].length)
-        this.buckvicaEvngl = rng2![0]
-
-
+        let rng2 = json[key].evngl.match(regx);
+        this.evngl = json[key].evngl.slice(rng2![0].length);
+        this.buckvicaEvngl = rng2![0];
       }
     }
-
   }
-
-
-
-
-
 }
-
